edf1cc9c2281b66d9d9e56fa32304c9a6e934852
MaggieBuxie <margaretmbua2015@gmail.com>
2024-10-13 20:25:38 +0300

    Implemented data model entities

---
+++ src/main/java/com/wellsfargo/counselor/entity/client.java
@@ -0,0 +1,62 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue()
+    private long clientId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "advisor_id", nullable = false)
+    private Advisor advisor;
+
+    protected Client() {}
+
+    public Client(String name, String email, Advisor advisor) {
+        this.name = name;
+        this.email = email;
+        this.advisor = advisor;
+    }
+
+    public Long getClientId() {
+        return clientId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Advisor getAdvisor() {
+        return advisor;
+    }
+
+    public void setAdvisor(Advisor advisor) {
+        this.advisor = advisor;
+    }
+}
